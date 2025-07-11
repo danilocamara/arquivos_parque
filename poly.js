441c27a912995ad7f8e97255a98510abdf874982
Danilo Câmara <danilocamara@gmail.com>
2025-07-11 14:32:29 -0300

    Update poly.js

--- poly.js
+++ poly.js
@@ -1,18 +1,18 @@
 polygon_callbak([{lat: -5.821084, lng: -35.208012},
-    {lat: -5.825928, lng: -35.194775},// 02	Av. Amintas Barros com Rua Themistócles Duarte
+    {lat: -5.825928, lng: -35.194775},
     {lat: -5.829226, lng: -35.192962},
     {lat: -5.826622, lng: -35.200816},
     {lat: -5.832460, lng: -35.202930},
     {lat: -5.836832, lng: -35.197401},
-    {lat: -5.840118, lng: -35.195179}, // instituto de fisica
+    {lat: -5.840118, lng: -35.195179},
     {lat: -5.841858, lng: -35.195007},
-    {lat: -5.842551, lng: -35.195071}, //ultima quina
+    {lat: -5.842551, lng: -35.195071},
     {lat: -5.843877, lng: -35.195762},
     {lat:-5.854602,lng: -35.200096 },
 	{lat:-5.849219,lng:-35.206816 },
 	{lat:-5.849132,lng: -35.206698 },
-	{lat:-5.838467,lng: -35.211352 }, //Av. Sen. Salgado Filho com Rua Ataulfo Alves
-	{lat:-5.838966,lng: -35.215169 }, //Rua Ataulfo Alves com Rua Frei Henrique Soares ,  
+	{lat:-5.838467,lng: -35.211352 },
+	{lat:-5.838966,lng: -35.215169 },  
 	{lat:-5.832844, lng:-35.216985 },
 	{lat:-5.832413, lng:-35.217600 },
 	{lat:-5.831890, lng:-35.217418 },
